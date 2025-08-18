f5f870b42552f9a6a9eda03e6abe01d7b4766e20
Tanu Singh <tanusingh28388@gmail.com>
2025-08-18 22:10:49 +0530

    support section done

--- src/landing_page/support/CreateTicket.js
+++ src/landing_page/support/CreateTicket.js
@@ -1,9 +1,209 @@
-import React from 'react';
+import React from "react";
 
 function CreateTicket() {
-    return ( 
-        <h1>createticket</h1>
-     );
+  return (
+    <div className="container">
+      <div className="row p-5 mt-5 mb-5">
+        <h1>To create a ticket, select a relevent topic</h1>
+      </div>
+      <div className="row p-5 mt-5 mb-5">
+        <div className="col">
+          <h4 className="mb-5 p-4">
+            {" "}
+            <i class="fa fa-plus-circle " aria-hidden="true"></i> Account Opening
+          </h4>
+          <ul style={{ listStyleType: "none" }}>
+            <li>
+              <a
+                href="#"
+                style={{ textDecoration: "none", lineHeight: "2.5em" }}
+              >
+                Online Account Opening
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                style={{ textDecoration: "none", lineHeight: "2.5em" }}
+              >
+                Offline Account Opening
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                style={{ textDecoration: "none", lineHeight: "2.5em" }}
+              >
+                Company, Partnership and HUF Account Opening
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                style={{ textDecoration: "none", lineHeight: "2.5em" }}
+              >
+                NRI Account Opening
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                style={{ textDecoration: "none", lineHeight: "2.5em" }}
+              >
+                Charges at Zerodha
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                style={{ textDecoration: "none", lineHeight: "2.5em" }}
+              >
+                Zerodha IDFC FIRST Bank 3-in-1 Account
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                style={{ textDecoration: "none", lineHeight: "2.5em" }}
+              >
+                Getting Started
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col">
+          <h4 className="mb-5 p-4">
+            {" "}
+            <i class="fa fa-user" aria-hidden="true"></i> Your Zerodha Account
+          </h4>
+          <ul style={{ listStyleType: "none" }}>
+            <li>
+              <a
+                href="#"
+                style={{ textDecoration: "none", lineHeight: "2.5em" }}
+              >
+                Login Credintials
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                style={{ textDecoration: "none", lineHeight: "2.5em" }}
+              >
+                Account Modification and Segement Addition
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                style={{ textDecoration: "none", lineHeight: "2.5em" }}
+              >
+                DP ID and bank details
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                style={{ textDecoration: "none", lineHeight: "2.5em" }}
+              >
+                Your Profile
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                style={{ textDecoration: "none", lineHeight: "2.5em" }}
+              >
+                Transfer and conversion of shares
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col">
+          <h4 className="mb-5 p-4">
+            <i class="fa fa-bar-chart" aria-hidden="true"></i>
+            Your Zerodha Account
+          </h4>
+          <ul style={{ listStyleType: "none" }}>
+            <li>
+              <a
+                href="#"
+                style={{ textDecoration: "none", lineHeight: "2.5em" }}
+              >
+                Margin/leverage, Product and Other types
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                style={{ textDecoration: "none", lineHeight: "2.5em" }}
+              >
+                Kite Web and Mobile
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                style={{ textDecoration: "none", lineHeight: "2.5em" }}
+              >
+                Trading FAQs
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                style={{ textDecoration: "none", lineHeight: "2.5em" }}
+              >
+                Corporate Actions
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                style={{ textDecoration: "none", lineHeight: "2.5em" }}
+              >
+                Sentinel
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                style={{ textDecoration: "none", lineHeight: "2.5em" }}
+              >
+                Kite API
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                style={{ textDecoration: "none", lineHeight: "2.5em" }}
+              >
+                Pi and other Platform
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                style={{ textDecoration: "none", lineHeight: "2.5em" }}
+              >
+                {" "}
+                Stockreports+
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                style={{ textDecoration: "none", lineHeight: "2.5em" }}
+              >
+                {" "}
+                GTT
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CreateTicket;
--- src/landing_page/support/Hero.js
+++ src/landing_page/support/Hero.js
@@ -2,9 +2,34 @@ import React from 'react';
 
 function Hero() {
     return (
-        <h1>
-            Hero
-        </h1>
+        <section className='container-fluid' id='supportHero'>
+            
+            <div className=' p-5 mt-5 ' id='supportWrapper'>
+                
+                    <h4>Support Portal</h4>
+                    <a href='#'>Track Tickets</a>
+                
+            </div>
+            <div className='row p-5 mx-5'>
+                <div className='col p-5'>
+                    <h2 className='fs-3'>Search for an answer or browser help topics <br/> to create a ticket</h2>
+                    <input placeholder='Eg. how do i activate F&O, why is my order getting rejected...' className='mt-3 mb-2'></input>
+                    <br/>
+                    <a href='#'>Track account opening</a>
+                    <a href='#'>Track segmentation activation</a>
+                    <a href='#'>Intrday.</a>
+                    <br/>
+                    <a href='#'>margins</a>
+                    <a href='#'>Kite user manual</a>
+
+                </div>
+                <div className='col p-5'>
+                    <h2>Featured</h2>
+                    <p>1. <a href='#'>Current Takeovers and Delisting- January 2025</a></p>
+                    <p>2. <a href='#'>Latest Intraday leverages - MIS & CO</a></p>
+                </div>
+            </div>
+        </section>
       );
 }
 
